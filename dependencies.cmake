460a27fc7a2305aa90e23a5d4c97621c799f00ba
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-02-03 18:24:01 -0500

    cmake: update avendish version

--- dependencies.cmake
+++ dependencies.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   avendish
   GIT_REPOSITORY "https://github.com/celtera/avendish"
-  GIT_TAG  6d67263 
+  GIT_TAG  e50ccb780defc3fd73046b052a2883e51470856d
   GIT_PROGRESS true
 )
 FetchContent_Populate(avendish)
